06e571eaa0554d32591bdefaf91474132092300f
gromley111 <gromley111@gmail.com>
2023-08-09 20:59:32 -0500

    add prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
-import './App.scss'
-import { useFetch } from './shared/hooks';
-import { Client, Clients } from './shared/models';
+import "./App.scss";
+import { useFetch } from "./shared/hooks";
+import { Client, Clients } from "./shared/models";
 
 function App() {
-    const { data, loading, error } = useFetch < Array < Client >> ('http://localhost:4200/clients');
-    console.log(error)
-    return (
-        <>
-            {data?.map((client) => <p> {client.firstName}</p>)}
-        </>
-    )
+  const { data, loading, error } = useFetch<Array<Client>>(
+    "http://localhost:4200/clients"
+  );
+
+  return (
+    <>
+      {data?.map((client) => (
+        <p>{client.firstName}</p>
+      ))}
+    </>
+  );
 }
 
-export default App
+export default App;
--- client/src/shared/hooks/use-fetch-two.hook.ts
+++ client/src/shared/hooks/use-fetch-two.hook.ts
@@ -3,48 +3,47 @@ import { useEffect, useState, useCallback } from "react";
 type Error = Response | string | null | undefined;
 
 interface ApiResponse<T> {
-    data: T | null;
-    error: Error;
-    loading: boolean;
+  data: T | null;
+  error: Error;
+  loading: boolean;
 }
 
 const DEFAULT_OPTIONS: RequestInit = {
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}
-
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 
 export function useFetch<T>(
-    url: string,
-    options: RequestInit = DEFAULT_OPTIONS
+  url: string,
+  options: RequestInit = DEFAULT_OPTIONS
 ): ApiResponse<T> {
-    const [data, setData] = useState<T | null>(null);
-    const [error, setError] = useState<Error>();
-    const [loading, setLoading] = useState<boolean>(false);
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error>();
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const fetchData = useCallback(async () => {
-        setLoading(true);
+  const fetchData = useCallback(async () => {
+    setLoading(true);
 
-        try {
-            const response = await fetch(url, { headers: options.headers });
-            if (!response.ok) {
-                throw new Error("Request failed");
-            }
-            const json = await response.json();
-            setData(json);
-            setError(null);
-        } catch (error) {
-            const errorResponse = error as Response;
-            setError(errorResponse.statusText);
-        }
+    try {
+      const response = await fetch(url, { headers: options.headers });
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+      const json = await response.json();
+      setData(json);
+      setError(null);
+    } catch (error) {
+      const errorResponse = error as Response;
+      setError(errorResponse.statusText);
+    }
 
-        setLoading(false);
-    }, [url, options.headers]);
+    setLoading(false);
+  }, [url, options.headers]);
 
-    useEffect(() => {
-        fetchData();
-    }, [fetchData]);
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
-    return { data, error, loading };
+  return { data, error, loading };
 }
